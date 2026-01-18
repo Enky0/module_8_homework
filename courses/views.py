57c1e70359dfe65ff14d5eb260456f907135d063
Semyon Boikov <sem-bojkov@yandex.ru>
2026-01-18 09:20:02 +0300

    at courses/Views.py added PaymentListAPIView

---
+++ courses/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, generics
+from rest_framework.filters import OrderingFilter
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from courses.serializers import CourseSerializer, LessonSerializer, PaymentSerializer
+from courses.models import Course, Lesson, Payment
+
+
+# Create your views here.
+
+class CourseViewSet(viewsets.ModelViewSet):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
+
+class LessonCreateAPIView(CreateAPIView):
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(ListAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonUpdateAPIView(UpdateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonDestroyAPIView(DestroyAPIView):
+    queryset = Lesson.objects.all()
+
+
+class PaymentListAPIView(ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('paid_course', 'paid_lesson', 'payment_method')
+
+    ordering_fields = ['payment_date',]
