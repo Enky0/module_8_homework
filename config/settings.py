1ce779abd26ad7fd262a1a0c6cabcde57941725a
Semyon Boikov <sem-bojkov@yandex.ru>
2026-01-18 09:12:12 +0300

    updated settings.py

--- config/settings.py
+++ config/settings.py
@@ -44,9 +44,17 @@ INSTALLED_APPS = [
 
     'rest_framework',
 
+    'django_filters',
+
     'users',
     'courses',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
