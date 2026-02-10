9bad31b5ac8d7c2c02f52efa8f7aba878ef46a45
Semyon Boikov <sem-bojkov@yandex.ru>
2026-02-10 14:42:36 +0300

    added price field to Course and Lesson serializers

--- courses/serializers.py
+++ courses/serializers.py
@@ -5,10 +5,11 @@ from courses.validators import YouTubeURLValidator
 
 class LessonSerializer(serializers.ModelSerializer):
     video_url = serializers.URLField(validators=[YouTubeURLValidator('video_url')])
+    price = serializers.IntegerField(min_value=0)
 
     class Meta:
         model = Lesson
-        fields = ('name', 'description', 'preview', 'video_url', 'course', 'owner')
+        fields = ('id', 'name', 'description', 'preview', 'video_url', 'course', 'owner', 'price')
 
     preview = serializers.ImageField(required=False, allow_null=True)
 
@@ -17,6 +18,7 @@ class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
     is_subscribed = serializers.SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True)
+    price = serializers.IntegerField(min_value=0)
 
     def get_lesson_count(self, course):
         return course.lessons.count()
@@ -29,7 +31,7 @@ class CourseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
-        fields = ('name', 'preview', 'description', 'lesson_count', 'lessons', 'owner')
+        fields = ('id', 'name', 'preview', 'description', 'lesson_count', 'lessons', 'owner', 'is_subscribed', 'price')
 
     preview = serializers.ImageField(required=False, allow_null=True)
 
